af767f06030e5f2bb38806f378c99c66e07ca99b
Martin Hrubes <m_hrubes@centrum.cz>
2026-02-22 01:20:01 +0100

    uprava

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -120,9 +120,9 @@ function Contact() {
             setIsLoading(true)
 
             // EmailJS konfigurace - tyto hodnoty musíte získat z EmailJS dashboardu
-            const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID
-            const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
-            const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+            const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID || ''
+            const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID || ''
+            const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY || ''
 
             // Kontrola, zda jsou nastaveny environment variables
             if (!serviceId || !templateId || !publicKey) {
